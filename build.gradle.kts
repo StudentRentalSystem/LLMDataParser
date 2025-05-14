c558faf9ccd969b74506e524636ac9cc424b55f6
Hding49 <87240249+hding4915@users.noreply.github.com>
2025-05-14 09:51:18 +0800

    Delete unneeded vector database, publish new package v1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,12 +23,6 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.json:json:20231013")
-    implementation("io.milvus:milvus-sdk-java:2.5.7")
-    implementation("org.slf4j:slf4j-api:1.7.32")
-    implementation("org.slf4j:slf4j-simple:1.7.32")
-    implementation("org.apache.lucene:lucene-core:8.9.0")
-    implementation("org.apache.lucene:lucene-analyzers-common:8.9.0")
-    implementation("org.apache.lucene:lucene-queryparser:8.9.0")
 }
 
 tasks.test {
--- src/main/java/io/github/studentrentalsystem/Settings.java
+++ src/main/java/io/github/studentrentalsystem/Settings.java
@@ -2,9 +2,6 @@ package io.github.studentrentalsystem;
 
 public final class Settings {
     public static final String promptPath = "extract_prompt.txt";
-    public static final String ragPromptPath = "rag_prompt.txt";
-    public static final String queryPromptPath = "query_prompt.txt";
-    public static final String mongoDBQueryPromptPath = "mongo_query_prompt.txt";
     public static final String rentalPostsPath = "rental_posts.json";
     public static final String extractedDataPath = "extracted_data.json";
     public static final String outputPath = "src/main/resources/" + extractedDataPath;
