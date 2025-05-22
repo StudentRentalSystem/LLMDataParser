31f611b44369d36ae7675e4033b604b51c42719a
Hding49 <87240249+hding4915@users.noreply.github.com>
2025-05-22 14:52:06 +0800

    Update RentalExtractor.java and add some javadoc

--- src/main/java/io/github/studentrentalsystem/RentalExtractor.java
+++ src/main/java/io/github/studentrentalsystem/RentalExtractor.java
@@ -14,10 +14,33 @@ import static io.github.studentrentalsystem.Settings.*;
 import static io.github.studentrentalsystem.Utils.getStringJSON;
 
 
+/**
+ * <p>
+ * This class is used to parse rental posts to structural json data <br>
+ * @author hding4915
+ */
 public class RentalExtractor {
+    private final int maxErrorTimes;
     private static String promptTemplate;
 
+
+    /**
+     * <p>
+     * Default maxErrorTimes will be set to 5
+     * @throws IOException if it gets error when reading the prompt file
+     */
     public RentalExtractor() throws IOException{
+        this(5);
+    }
+
+
+    /**
+     *
+     * @param maxErrorTimes set the maxErrorTimes for parsing the rental posts
+     * @throws IOException if it gets error when reading the prompt file
+     */
+    public RentalExtractor(int maxErrorTimes) throws IOException{
+        this.maxErrorTimes = maxErrorTimes;
         parseResData();
     }
 
@@ -40,12 +63,11 @@ public class RentalExtractor {
 
     /**
      * <p>
-     *     This function will call local LLM model to generate structural JSON post
-     * </p>
-     * @param post - The rental post
-     * @param model - The model type you want the LLM be.
+     * This function will call local LLM model to generate structural json data
+     * @param post the rental post
+     * @param model the LLM model type to use
      * @see LLMClient.ModelType
-     * @return Type of {@link JSONObject} structural post
+     * @return type of {@link JSONObject} structural post
      */
     public JSONObject getJSONPost(String post, LLMClient.ModelType model) throws JSONException{
         String response = callLocalModel(post, model);
@@ -65,9 +87,79 @@ public class RentalExtractor {
         }
     }
 
+    private boolean checkFirstObject(JSONArray jsonArray) {
+        return (jsonArray.length() == 1 && (jsonArray.getString(0).isEmpty() || jsonArray.getString(0).equals("未知")));
+    }
+
+    private void writeDefaultEmpty(JSONObject jsonObject, String key, Object value) {
+        if (!jsonObject.has(key)) jsonObject.put(key, value);
+    }
+
+    /**
+     * <p>
+     * This function is used to adjust the structural json data to a fixed format
+     * @param structural_post one rental post json object
+     */
+    public void formSameJSON(JSONObject structural_post) {
+        JSONArray contact = structural_post.getJSONArray("聯絡方式");
+        JSONArray result = new JSONArray();
+        for (int i = 0; i < contact.length(); i++) {
+            JSONObject personal_contact = contact.getJSONObject(i);
+            writeDefaultEmpty(personal_contact, "聯絡人", "");
+            writeDefaultEmpty(personal_contact, "手機", new JSONArray());
+            writeDefaultEmpty(personal_contact, "lineID", new JSONArray());
+            writeDefaultEmpty(personal_contact, "lineLink", new JSONArray());
+            writeDefaultEmpty(personal_contact, "others", new JSONArray());
+
+            String name = personal_contact.getString("聯絡人");
+            JSONArray phone_nums = personal_contact.getJSONArray("手機");
+            JSONArray lineID = personal_contact.getJSONArray("lineID");
+            JSONArray lineLink = personal_contact.getJSONArray("lineLink");
+            JSONArray others = personal_contact.getJSONArray("others");
+
+            if (name.equals("未知")) name = "";
+            if (checkFirstObject(phone_nums)) {
+                phone_nums = new JSONArray();
+            } else {
+                JSONArray modified = new JSONArray();
+                for (int j = 0; j < phone_nums.length(); j++) {
+                    String phone = phone_nums.getString(j);
+                    if (phone.contains("-")) modified.put(phone.replace("-", ""));
+                    else modified.put(phone);
+                }
+                phone_nums = modified;
+            }
+            if (checkFirstObject(lineID)) lineID = new JSONArray();
+            if (checkFirstObject(lineLink)) lineLink = new JSONArray();
+            if (checkFirstObject(others)) others = new JSONArray();
+            personal_contact = new JSONObject();
+            personal_contact.put("聯絡人", name);
+            personal_contact.put("手機", phone_nums);
+            personal_contact.put("lineID", lineID);
+            personal_contact.put("lineLink", lineLink);
+            personal_contact.put("others", others);
+            result.put(personal_contact);
+        }
+        structural_post.put("聯絡方式", result);
+    }
+
+
+    /**
+     * <p>
+     * This function is used to parse single rental post to structural json data. <br>
+     * When getting errors, it will parse again until the error times >= maxErrorTimes.
+     * @param post single rental post string
+     * @param model the LLM model type to use
+     * @see LLMClient.ModelType
+     * @return structural json post, type of {@link JSONObject}
+     */
     public JSONObject getJSONPostNoError(String post , LLMClient.ModelType model) {
         JSONObject jsonPost;
-        while ((jsonPost = noErrorJSONPost(post, model)) == null);
+        int errorTimes = 0;
+        while ((jsonPost = noErrorJSONPost(post, model)) == null && errorTimes < maxErrorTimes) {
+            errorTimes++;
+        }
+        if (jsonPost != null) formSameJSON(jsonPost);
         return jsonPost;
     }
 
@@ -84,6 +176,15 @@ public class RentalExtractor {
         return revisedRental;
     }
 
+
+    /**
+     * <p>
+     * This function is used to parse multiple rental posts to structural json data
+     * @param inputPath input file path of the original rental posts, is a .json file that contains ["post1", "post2", ...]
+     * @param outputPath output file path of the structural json data, is a .json file
+     * @param model the LLM model type to use
+     * @see LLMClient.ModelType
+     */
     public void processPosts(String inputPath, String outputPath, LLMClient.ModelType model) {
         try {
             // Read all posts from input JSON file
@@ -98,6 +199,8 @@ public class RentalExtractor {
 
                 JSONObject parsed = getJSONPostNoError(post, model);
 
+                formSameJSON(parsed);
+
                 results.add(parsed);
             }
 
@@ -111,27 +214,19 @@ public class RentalExtractor {
         }
     }
 
+    /**
+     * <p>
+     * This function is used to get the LLM response when converting the rental post to structural json data.
+     * @param text the rental post
+     * @param model the LLM model type to use
+     * @see LLMClient.ModelType
+     * @return type of {@link String} response
+     */
     public String callLocalModel(String text, LLMClient.ModelType model) {
         String prompt = promptTemplate.replace("{text}", text);
 
         String response = LLMClient.callLocalModel(prompt, model, "http://localhost:11434/api/generate");
         JSONObject jsonResponse = new JSONObject(response.toString());
         return jsonResponse.getString("response");
-    }
-
-    public JSONObject getDefaultUnknownJson() {
-        JSONObject obj = new JSONObject();
-
-        obj.put("地點", "未知");
-        obj.put("租金", "未知");
-        obj.put("坪數", "未知");
-        obj.put("格局", "未知");
-        obj.put("是否可養寵物", "未知");
-        obj.put("可否開伙", "未知");
-        obj.put("性別限制", "未知");
-        obj.put("是否有電梯", "未知");
-        obj.put("聯絡方式", "未知");
-        obj.put("照片", "未知");
-        return obj;
     }
 }
