5733e0bbaffe80900e93c0ff0c11a2ca00ffe5a9
Hding49 <87240249+hding4915@users.noreply.github.com>
2025-06-13 11:06:11 +0800

    Remove RentalExtrator

--- src/main/java/io/github/studentrentalsystem/Settings.java
+++
@@ -1,9 +0,0 @@
-package io.github.studentrentalsystem;
-
-public final class Settings {
-    public static final String promptPath = "extract_prompt.txt";
-    public static final String rentalPostsPath = "rental_posts.json";
-    public static final String extractedDataPath = "extracted_data.json";
-    public static final String outputPath = "src/main/resources/" + extractedDataPath;
-
-}
